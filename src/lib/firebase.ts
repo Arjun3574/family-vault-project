e25a98fd9ad69c32c6ba4bf57c1f1c7acf83004a
Arjun Mankuskar <mankuskararjun@gmail.com>
2025-11-10 13:42:03 +0000

    for aAuthentication use firebase only

--- src/lib/firebase.ts
+++
@@ -1,8 +0,0 @@
-// This file is obsolete and can be removed.
-// All firebase initialization is now handled by the provider system.
-// Keeping it for now to avoid breaking imports, but it should be deprecated.
-import { initializeFirebase } from '@/firebase';
-
-const { firebaseApp, auth, firestore, storage } = initializeFirebase();
-
-export { firebaseApp as app, auth, firestore as db, storage };
