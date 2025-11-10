825690ac5e14f8ced3127e9a9ae80e1c6374ba7b
Arjun Mankuskar <mankuskararjun@gmail.com>
2025-11-10 13:26:02 +0000

    failed

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     // This is required to allow the Next.js dev server to accept requests from the
     // Firebase Studio environment.
     allowedDevOrigins: [
-      'https://6000-firebase-studio-1761983285994.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev',
+      'https://6000-firebase-studio-1761983285994.cluster-73qgvk7hjjadkrjeyexca5ivva.dev',
     ],
   },
   typescript: {
